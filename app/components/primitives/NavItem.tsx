db0ccd1679d425ae9fecb8ff10a71006fd23adaa
Daniil Polienko <donyaschefer@gmail.com>
2025-02-03 19:09:36 +0300

    style: nav hover

--- app/components/primitives/NavItem.tsx
+++ app/components/primitives/NavItem.tsx
@@ -6,7 +6,7 @@ export function NavItem({title, href}: {title: string; href: string}): ReactNode
 			<div className={'group relative'}>
 				<div
 					className={
-						'absolute inset-0 origin-center rounded-3xl bg-[rgba(12,12,12,0.03)] backdrop-blur-[32px] transition-all group-hover:scale-90'
+						'absolute inset-0 origin-center rounded-3xl bg-[rgba(12,12,12,0.03)] backdrop-blur-[32px] transition-all group-hover:scale-90 group-hover:bg-[rgba(12,12,12,0.07)]'
 					}
 				/>
 				<button className={'relative z-10 px-6 py-4 font-extrabold leading-8'}>{title}</button>
